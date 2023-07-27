0bda5ade37d9e31c63b7479fdc79e731f3c6263c
Agr-Vaibhav <134123725+Agr-Vaibhav@users.noreply.github.com>
2023-07-27 18:10:20 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const {Pool}=require('pg')
 require('dotenv').config()
 
 const pool=new Pool({
-    user:"postgres",
-    password:"abcd",
-    host:"localhost",
-    port:5432,
+    user:process.env.USERNAME,
+    password:process.env.PASSWORD,
+    host:process.env.HOST,
+    port:process.env.DBPORT,
     database:'todoapp'
 });
 module.exports=pool

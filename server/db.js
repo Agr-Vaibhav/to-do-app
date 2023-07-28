ca2b4887412b6143dd91de0b5a6b68693bc7d50f
Agr-Vaibhav <134123725+Agr-Vaibhav@users.noreply.github.com>
2023-07-28 10:30:10 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,9 +1,14 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log('USERNAME:', process.env.userNme);
+console.log('PASSWORD:', process.env.passWrd);
+console.log('HOST:', process.env.HOST);
+console.log('DBPORT:', process.env.DBPORT);
+
 const pool = new Pool({
-  user:'KyOIjw8iyjQOQPOd',
-  password: process.env.PASSWORD,
+  user: process.env.userNme,
+  password: process.env.passWrd,
   host: process.env.HOST,
   port: process.env.DBPORT,
   database:'todoapp',
